178984b64fb688c9c8cb6459c47889db9e42c0a3
jtsinghua <zqh198971@hotmail.com>
2019-12-20 10:16:27 +0800

    create maven project

---
+++ src/main/java/com/freetsinghua/simplejavaapp/SimpleJavaApp.java
@@ -0,0 +1,15 @@
+package com.freetsinghua.simplejavaapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author octopus
+ */
+@SpringBootApplication
+public class SimpleJavaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleJavaApp.class, args);
+    }
+}
